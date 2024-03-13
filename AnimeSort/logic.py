1b4b70ca4d7f9cada3781f0b8d063cee6700de32
CorlisHunt <132340197+CorlisHunt@users.noreply.github.com>
2024-03-13 13:21:14 +0400

    Update logic.py

--- AnimeSort/logic.py
+++ AnimeSort/logic.py
@@ -5,6 +5,10 @@ import os
 
 def process_folder(folder_path):
     print(f"Обработка папки: {folder_path}")
+        for root, dirs, filenames in os.walk(folder_path):
+        print(f"root: {root}")
+        print(f"dirs: {dirs}")
+        print(f"filenames: {filenames}")
     # for root, dirs, files in os.walk(folder_path):
     #     print(root, dirs, files)
     #     for file in files:
